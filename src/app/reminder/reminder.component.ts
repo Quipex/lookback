30e2ddd182e30cf927a932d028e84ce431272195
Quipex <rudas72@gmail.com>
2019-02-15 13:39:54 +0200

    create core component

---
+++ src/app/reminder/reminder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qpx-reminder',
+  templateUrl: './reminder.component.html',
+  styleUrls: ['./reminder.component.scss']
+})
+export class ReminderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
